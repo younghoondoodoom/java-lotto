2e8ec55bb7df1cd781d5b72bad2203d870b735d2
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-14 20:47:56 +0900

    test(LottoStatistic): 로또 통계에서 수익률 계산 기능 및 통계에 데이터 넣는 기능
    수익률 계산 기능:
    - 정확한 계산을 위해 BigDecimal 사용
    - 소수점 둘째 자리에서 반올림
    - %로 표시하기 위해 100을 곱함
    통계에 데이터 넣는 기능:
    - 응집를 높이기 위한 데이터 넣는 기능 추가

--- src/main/java/lotto/dto/LottoStatistic.java
+++ src/main/java/lotto/dto/LottoStatistic.java
@@ -1,6 +1,7 @@
 package lotto.dto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 import lotto.type.LottoStandard;
@@ -9,6 +10,7 @@ public class LottoStatistic {
 
     private final Map<LottoStandard, Integer> resultMap;
     private BigDecimal yield;
+    private static final int DECIMAL_POINT = 1;
 
     public LottoStatistic() {
         resultMap = new HashMap<>();
@@ -23,5 +25,15 @@ public class LottoStatistic {
 
     public BigDecimal getYield() {
         return yield;
+    }
+
+    public void addLottoStandard(LottoStandard lottoStandard) {
+        resultMap.replace(lottoStandard, resultMap.get(lottoStandard) + 1);
+    }
+
+    public void calculateYield(long purchaseAmount, long totalPrize) {
+        this.yield = BigDecimal.valueOf(totalPrize)
+            .multiply(BigDecimal.valueOf(100L))
+            .divide(BigDecimal.valueOf(purchaseAmount), DECIMAL_POINT, RoundingMode.HALF_UP);
     }
 }
