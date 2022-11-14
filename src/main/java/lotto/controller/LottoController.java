a5cc172489a4c5e607c239f9b6532b694ac456b1
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-15 06:54:57 +0900

    feat(LottoController): 로또 전체 흐름을 관리하는 Controller 구현
    LottoController 역할:
    - 전체 흐름 관리
    - 값 입출력 관리
    - Validator로 입력 값 연결
    - Service로 입력 값 연결

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,75 @@
+package lotto.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.Lotto;
+import lotto.dto.LottoStatistic;
+import lotto.service.LottoService;
+import lotto.type.LottoInformation;
+import lotto.validator.integrated.BonusNumberInputValidator;
+import lotto.validator.integrated.PurchaseAmountInputValidator;
+import lotto.validator.integrated.WinningNumberInputValidator;
+import lotto.view.LottoInputView;
+import lotto.view.LottoOutputView;
+
+public class LottoController {
+
+    private final LottoService lottoService;
+    private final BonusNumberInputValidator bonusNumberInputValidator;
+    private final PurchaseAmountInputValidator purchaseAmountInputValidator;
+    private final WinningNumberInputValidator winningNumberInputValidator;
+
+    public LottoController(LottoService lottoService, BonusNumberInputValidator bonusNumberInputValidator,
+        PurchaseAmountInputValidator purchaseAmountInputValidator,
+        WinningNumberInputValidator winningNumberInputValidator) {
+        this.lottoService = lottoService;
+        this.bonusNumberInputValidator = bonusNumberInputValidator;
+        this.purchaseAmountInputValidator = purchaseAmountInputValidator;
+        this.winningNumberInputValidator = winningNumberInputValidator;
+    }
+
+    public void play() {
+        long purchaseAmount = getPurchaseAmount();
+
+        List<Lotto> lottos = getLottos(purchaseAmount);
+        LottoOutputView.outputLottos(lottos);
+
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber();
+
+        LottoStatistic statistic = lottoService.makeStatistic(lottos, purchaseAmount, winningNumbers, bonusNumber);
+        LottoOutputView.outputLottoStatistic(statistic);
+    }
+
+    private long getPurchaseAmount() {
+        String purchaseAmountInput = LottoInputView.inputPurchaseAmount();
+        if (!purchaseAmountInputValidator.validate(purchaseAmountInput)) {
+            throw new IllegalArgumentException();
+        }
+        return Long.parseLong(purchaseAmountInput);
+    }
+
+    private List<Lotto> getLottos(long purchaseAmount) {
+        int size = (int) (purchaseAmount / LottoInformation.SMALLEST_UNIT.value());
+        return lottoService.createLottos(size);
+    }
+
+    private List<Integer> getWinningNumbers() {
+        String winningNumbersInput = LottoInputView.inputWinningNumbers();
+        List<String> winningNumbers = Arrays.stream(winningNumbersInput.split(",")).collect(Collectors.toList());
+        if (!winningNumberInputValidator.validate(winningNumbers)) {
+            throw new IllegalArgumentException();
+        }
+        return winningNumbers.stream().map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private int getBonusNumber() {
+        String bonusNumberInput = LottoInputView.inputBonusNumber();
+        if (!bonusNumberInputValidator.validate(bonusNumberInput)) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(bonusNumberInput);
+    }
+
+}
