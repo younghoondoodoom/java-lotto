39282f6857062a3eb0616efabe25e31fc1b47800
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-15 18:03:19 +0900

    fix(NumericValidator): 입력 값에 null이나 빈 값 들어올 경우 오류 수정

--- src/main/java/lotto/validator/NumericValidator.java
+++ src/main/java/lotto/validator/NumericValidator.java
@@ -3,6 +3,6 @@ package lotto.validator;
 public interface NumericValidator {
 
     default boolean isNumeric(String target) {
-        return target.chars().allMatch(Character::isDigit);
+        return target != null && !target.equals("") && target.chars().allMatch(Character::isDigit);
     }
 }
