8fe2deb126d50b1433d8e4cffe1c046a97685426
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-15 17:51:53 +0900

    refactor(LottoOutputView): formatter 수정
    각 숫자의 formatter를 달리하기 위해 로직 수정

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -10,6 +10,8 @@ import lotto.type.LottoStandard;
 
 public class LottoOutputView {
 
+    private static final DecimalFormat formatter = new DecimalFormat();
+
     public static void outputLottos(final List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
@@ -36,8 +38,8 @@ public class LottoOutputView {
         printResultMap(statistic, lottoStandards);
     }
 
-    private static void printResultMap(LottoStatistic statistic, List<LottoStandard> lottoStandards) {
-        DecimalFormat amountFormatter = new DecimalFormat("###,###");
+    private static void printResultMap(LottoStatistic statistic,
+        List<LottoStandard> lottoStandards) {
         for (LottoStandard lottoStandard : lottoStandards) {
             if (lottoStandard.equals(LottoStandard.NOTHING)) {
                 continue;
@@ -47,7 +49,8 @@ public class LottoOutputView {
             if (lottoStandard.equals(LottoStandard.SECOND)) {
                 System.out.print(", 보너스 볼 일치");
             }
-            System.out.print(" (" + amountFormatter.format(lottoStandard.getPrize()) + "원) - ");
+            formatter.applyPattern("###,###");
+            System.out.print(" (" + formatter.format(lottoStandard.getPrize()) + "원) - ");
             System.out.print(result + "개");
             System.out.println();
         }
@@ -55,6 +58,7 @@ public class LottoOutputView {
 
     private static void printLottoStatisticYield(LottoStatistic statistic) {
         System.out.print("총 수익률은 ");
-        System.out.print(statistic.getYield() + "%입니다.");
+        formatter.applyPattern("###,###.0");
+        System.out.print(formatter.format(statistic.getYield()) + "%입니다.");
     }
 }
